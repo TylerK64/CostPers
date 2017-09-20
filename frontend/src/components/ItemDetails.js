db519b164b9338190b5533d644668f91ed70ccbb
TylerK64 <TylerKawabata@gmail.com>
2017-09-20 01:35:17 -0700

    made a bunch of changes then deleted them. Was trying to add a scrollview. Pretty sure most of it got deleted.

--- frontend/src/components/ItemDetails.js
+++ frontend/src/components/ItemDetails.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import axios from 'axios';
-import { Card } from './common';
-import { Actions } from 'react-native-router-flux';
-import Star from './Star';
-import Icon from 'react-native-vector-icons/MaterialIcons' ;
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import axios from "axios";
+import { Card } from "./common";
+import { Actions } from "react-native-router-flux";
+import Star from "./Star";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 class ItemDetails extends Component {
-
   constructor(props) {
     super(props);
 
@@ -17,30 +16,34 @@ class ItemDetails extends Component {
     this.state = {
       userId: userId,
       itemId: itemId,
-      itemName: '',
-      uses: '',
-      price: '',
-      costPer: '',
+      itemName: "",
+      uses: "",
+      price: "",
+      costPer: "",
       star: star
     };
   }
 
   componentWillMount() {
-    axios.get('https://sheltered-peak-36785.herokuapp.com/users/' + this.state.userId + '/items/' + this.state.itemId)
-    .then((response) => {
-      this.setState(
-        {
+    axios
+      .get(
+        "https://sheltered-peak-36785.herokuapp.com/users/" +
+          this.state.userId +
+          "/items/" +
+          this.state.itemId
+      )
+      .then(response => {
+        this.setState({
           itemName: response.data.name,
           uses: response.data.used_times,
           price: response.data.price,
           costPer: response.data.costper.costper,
           star: response.data.star
-        }
-      );
-      console.log(response);
-    })
-    .catch((error) => console.log(error));
-  };
+        });
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
 
   // const toggleStar=toggle?"star":"star-border";
 
@@ -48,24 +51,24 @@ class ItemDetails extends Component {
     console.log(this.state.star);
     return (
       <View style={styles.container}>
-
         <View style={styles.itemNameContainer}>
           <Text style={styles.itemNameText}>{this.state.itemName}</Text>
         </View>
 
         <View style={styles.timesUsedContainer}>
-            <Text style={styles.timesUsedText}>Uses: </Text>
-            <Text style={styles.timesUsedValue}>{this.state.uses}</Text>
+          <Text style={styles.timesUsedText}>Uses: </Text>
+          <Text style={styles.timesUsedValue}>{this.state.uses}</Text>
         </View>
 
         <View style={styles.priceContainer}>
-            <Text style={styles.priceText}>Price: </Text><Text style={styles.priceValue}>${this.state.price}</Text>
+          <Text style={styles.priceText}>Price: </Text>
+          <Text style={styles.priceValue}>${this.state.price}</Text>
         </View>
 
         <View style={styles.bigCostperContainer}>
           <View style={styles.currentCostperContainer}>
-              <Text style={styles.costperWord}>Costper</Text>
-              <Text style={styles.currentCostperText}>use: </Text>
+            <Text style={styles.costperWord}>Costper</Text>
+            <Text style={styles.currentCostperText}>use: </Text>
           </View>
           <View style={styles.costperMoneyContainer}>
             <Text style={styles.costperDollarSign}> $</Text>
@@ -80,20 +83,19 @@ class ItemDetails extends Component {
             star={this.state.star}
           />
         </Card>
-
       </View>
     );
   }
 }
 
-const styles= ({
+const styles = {
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   itemNameContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 20,
     height: 200,
     width: 300
@@ -104,106 +106,106 @@ const styles= ({
     marginRight: 15,
     marginLeft: 15,
     width: 290,
-    color: '#16795B',
-    textAlign: 'center',
-    flexWrap: 'wrap',
+    color: "#16795B",
+    textAlign: "center",
+    flexWrap: "wrap",
     adjustsFontSizeToFit: true
   },
   timesUsedContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 40,
     width: 320,
     height: 40
   },
   timesUsedText: {
-    color: 'black',
+    color: "black",
     fontSize: 24,
     width: 150,
     height: 40,
-    textAlign: 'right',
+    textAlign: "right",
     marginRight: 10
   },
   timesUsedValue: {
-    color: '#16795B',
+    color: "#16795B",
     fontSize: 32,
     width: 150,
     height: 40,
-    textAlign: 'left',
+    textAlign: "left",
     marginLeft: 10
   },
   priceContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     width: 320,
     height: 40,
     marginBottom: 30
   },
   priceText: {
-    color: 'black',
+    color: "black",
     fontSize: 24,
     width: 150,
-    textAlign: 'right',
+    textAlign: "right",
     marginRight: 10
   },
   priceValue: {
-    color: '#16795B',
+    color: "#16795B",
     width: 150,
     marginLeft: 10,
     fontSize: 32,
-    textAlign: 'left',
+    textAlign: "left",
     adjustsFontSizeToFit: true
   },
   bigCostperContainer: {
-    height: 100,
+    height: 100
   },
   currentCostperContainer: {
-    flexDirection: 'row',
-    height: 30,
+    flexDirection: "row",
+    height: 30
   },
   currentCostperText: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 24
   },
   costperWord: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 24,
     marginRight: 4,
     marginLeft: 5,
-    color: '#16795B'
+    color: "#16795B"
   },
   costperMoneyContainer: {
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   costperDollarSign: {
     fontSize: 40,
-    color: '#2f4f4f',
+    color: "#2f4f4f",
     marginTop: 1
   },
   currentCostperValue: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 60,
-    color: '#2f4f4f',
-    marginTop: 1,
+    color: "#2f4f4f",
+    marginTop: 1
   },
   starContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 30,
     marginBottom: 30,
     borderRadius: 10,
     width: 200,
     height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgb(255, 202, 58)'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(255, 202, 58)"
   },
   starText: {
-    textAlign: 'center',
-    color: 'white',
+    textAlign: "center",
+    color: "white",
     fontSize: 18,
     borderRadius: 5
   }
-})
+};
 
 export default ItemDetails;
