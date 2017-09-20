c301a5d8ea4e28e54f4b45ae71afc19757b215d3
TylerK64 <TylerKawabata@gmail.com>
2017-09-20 00:20:00 -0700

    add linux library dependencies to lock files & App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View, Image } from "react-native";
+import axios from "axios";
+import GlobalFont from "react-native-global-font";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
-import axios from 'axios';
-import GlobalFont from 'react-native-global-font';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Header, Footer, ItemsList, Spinner, TabIcon } from './components/common';
-import AddItem from './components/AddItem';
-import LoginForm from './components/LoginForm';
-import { Scene, Router, Actions, NavBar } from 'react-native-router-flux';
-import MainNavBar from './components/MainNavBar';
-import ItemDetails from './components/ItemDetails';
+  Header,
+  Footer,
+  ItemsList,
+  Spinner,
+  TabIcon
+} from "./components/common";
+import AddItem from "./components/AddItem";
+import LoginForm from "./components/LoginForm";
+import { Scene, Router, Actions, NavBar } from "react-native-router-flux";
+import MainNavBar from "./components/MainNavBar";
+import ItemDetails from "./components/ItemDetails";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
-      error: '',
+      email: "",
+      password: "",
+      error: "",
       loading: false,
-      userId: '',
-      firstName: '',
-      lastName: '',
-      itemId: '',
+      userId: "",
+      firstName: "",
+      lastName: "",
+      itemId: "",
       auth: false,
       data: []
     };
@@ -40,82 +40,83 @@ class App extends Component {
   }
 
   componentWillMount() {
-     let renogare = 'Renogare';
-     GlobalFont.applyGlobal(renogare);
+    let renogare = "Renogare";
+    GlobalFont.applyGlobal(renogare);
   }
 
   authentication(email, password) {
-    axios.post('https://sheltered-peak-36785.herokuapp.com/login', {
+    axios
+      .post("https://sheltered-peak-36785.herokuapp.com/login", {
         email: email,
         password: password
-    })
-    .then((response) => {
-       this.setState(
-      { userId: response.data.user_id,
-        firstName: response.data.first_name,
-        lastName: response.data.last_name,
-        loading: false,
-        auth: true
-      });
+      })
+      .then(response => {
+        this.setState({
+          userId: response.data.user_id,
+          firstName: response.data.first_name,
+          lastName: response.data.last_name,
+          loading: false,
+          auth: true
+        });
 
-    Actions.itemsList(
-        { userId: this.state.userId
-        }
+        Actions.itemsList({
+          userId: this.state.userId
+        });
+      })
+      .catch(() =>
+        this.setState({
+          error: "Login failed, please try again.",
+          loading: false
+        })
       );
-    })
-    .catch(() => this.setState(
-        { error: 'Login failed, please try again.',
-         loading: false
-      }
-      ));
   }
 
   render() {
-
     return (
-      <Router
-        sceneStyle={{ paddingTop: 65 }}
-      >
+      <Router sceneStyle={{ paddingTop: 65 }}>
         <Scene key="root">
           <Scene
-          key="login"
-          component={LoginForm}
-          authentication={this.authentication}
-          title="CostPers"
-          hideNavBar={true}
-          sceneStyle={{ paddingTop: 21 }}
-          initial
+            key="login"
+            component={LoginForm}
+            authentication={this.authentication}
+            title="CostPers"
+            hideNavBar={true}
+            sceneStyle={{ paddingTop: 21 }}
+            initial
           />
 
-        <Scene
-           key="itemsList"
-           title="CostPers"
-           component={ItemsList}
-           navigationBarStyle={styles.navBar}
-           renderBackButton={()=>(null)}
-           hideNavBar={false}
-           titleStyle={styles.navBarTextStyle}
-        />
+          <Scene
+            key="itemsList"
+            title="CostPers"
+            component={ItemsList}
+            navigationBarStyle={styles.navBar}
+            renderBackButton={() => null}
+            hideNavBar={false}
+            titleStyle={styles.navBarTextStyle}
+          />
 
-        <Scene
+          <Scene
             key="addItem"
             component={AddItem}
             title="Add Item"
             hideNavBar={false}
             titleStyle={styles.navBarTextStyle}
             leftButtonIconStyle={styles.navBarIconStyle}
-        />
+          />
 
-        <Scene
+          <Scene
             key="itemDetails"
             component={ItemDetails}
             title="Item Details"
             hideNavBar={false}
             titleStyle={styles.navBarTextStyle}
             leftButtonIconStyle={styles.navBarIconStyle}
-            onBack={() => Actions.itemsList(
-            { userId: this.state.userId, star: this.state.star })}
-        />
+            onBack={() =>
+              Actions.itemsList({
+                userId: this.state.userId,
+                star: this.state.star
+              })}
+          />
         </Scene>
       </Router>
     );
@@ -124,18 +125,18 @@ class App extends Component {
 
 const styles = {
   navBar: {
-    backgroundColor: '#F0F0F0',
+    backgroundColor: "#F0F0F0"
   },
   image: {
     width: 50,
     height: 50
   },
   navBarTextStyle: {
-    color: '#16795b',
+    color: "#16795b",
     fontSize: 23
   },
   navBarIconStyle: {
-    tintColor: '#16795b',
+    tintColor: "#16795b"
   }
 };
 
