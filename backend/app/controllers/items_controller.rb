ae28e2b112d47446ceb48237f54d23aaf7236484
TylerK64 <TylerKawabata@gmail.com>
2017-09-20 18:52:21 -0700

    Add two commented lines for items controller. Uncomment first line to disable auth token requirement for index and show pages for items.

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApiController
   before_action :require_login
+  # skip_before_action :verify_authenticity_token, only: [:index, :show], raise: false
+  # Can uncomment previous line if sending json data for get requests is desired; removes auth token requirement
 
   def index
     @user = User.find_by(id: params[:user_id])
